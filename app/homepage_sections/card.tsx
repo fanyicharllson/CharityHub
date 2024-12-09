40e40871690117dd9ee29c0a1b46216b04f6f250
fanyicharllson <fanyicharllson1@gmail.com>
2024-12-09 09:07:14 -0800

    added card section and some animations

---
+++ app/homepage_sections/card.tsx
@@ -0,0 +1,15 @@
+
+const cards = () => {
+  return (
+   <section>
+     <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
+      <div className="h-32 rounded-lg bg-gray-200"></div>
+      <div className="h-32 rounded-lg bg-gray-200"></div>
+      <div className="h-32 rounded-lg bg-gray-200"></div>
+      <div className="h-32 rounded-lg bg-gray-200"></div>
+    </div>
+   </section>
+  );
+};
+
+export default cards;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Banner from "./sections/banner"
-import SaveLife from "./sections/saveLife"
+import Banner from "./homepage_sections/banner"
+import SaveLife from "./homepage_sections/saveLife"
+import Card from "./homepage_sections/card"
 
 const Home = () => {
   return (
     <div>
       <Banner/>
       <SaveLife/>
+      <Card/>
     </div>
   )
 }
