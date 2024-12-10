a883fc47cf6a3e9ca8c9bcd27bdd965394477188
fanyicharllson <fanyicharllson1@gmail.com>
2024-12-10 12:33:58 -0800

    Added footer section

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer className="w-full py-8 text-white" style={{ background: "linear-gradient(135deg, #013a4a, #02a499, #a4f1e1)" }}>footer</footer>
   )
 }
 
