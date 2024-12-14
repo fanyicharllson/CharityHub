cf2fd83facea1f9e1b92d36e90f700281a7c1b1b
fanyicharllson <fanyicharllson1@gmail.com>
2024-12-14 15:20:11 -0800

    Made some change

--- app/donate/[donateid]/page.tsx
+++ app/donate/[donateid]/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { supabase } from "@/utils/supabaseClient";
 import Image from "next/image";
 import NotFoundPageCustom from "@/app/notfoundpage_custom/page";
+import SkeletonLoader from "@/components/skeletonLoader";
 
 const DonatePage = ({ params }: { params: Promise<{ donateid: string }> }) => {
   const [cause, setCause] = useState<any>(null);
@@ -14,14 +15,14 @@ const DonatePage = ({ params }: { params: Promise<{ donateid: string }> }) => {
   useEffect(() => {
     const fetchParams = async () => {
       const resolvedParams = await params;
-      setDonateid(resolvedParams.donateid); // Extract donateid from params
+      setDonateid(resolvedParams.donateid);
     };
 
     fetchParams();
   }, [params]);
 
   useEffect(() => {
-    if (donateid === null) return; // Wait until donateid is set
+    if (donateid === null) return;
 
     const causeId = Number(donateid);
 
@@ -46,7 +47,7 @@ const DonatePage = ({ params }: { params: Promise<{ donateid: string }> }) => {
         }
 
         setCause(data);
-        setError(null); // Clear any previous error
+        setError(null);
       } catch (err: any) {
         setError("The requested cause could not be found.");
         setCause(null);
@@ -60,36 +61,49 @@ const DonatePage = ({ params }: { params: Promise<{ donateid: string }> }) => {
 
   if (loading) {
     return (
-      <div className="max-w-screen-lg mx-auto py-10 px-4">
-        <p>Loading...</p>
+      <div className="max-w-screen-lg mx-auto py-32 px-4">
+        <SkeletonLoader />
       </div>
     );
   }
 
   return (
-    <div className="max-w-screen-lg mx-auto py-32 px-4">
-      {error ? (
-       <NotFoundPageCustom/>
-      ) : (
-        <>
-          <h1 className="text-4xl font-bold mb-4">{cause.title}</h1>
-          <p className="text-gray-600 mb-4">{cause.category}</p>
-          <Image
-            src={cause.image_url}
-            alt={cause.title}
-            width={800}
-            height={400}
-            className="rounded-md"
-          />
-          <p className="mt-6 text-lg">{cause.description}</p>
-          <div className="mt-10">
-            <button className="bg-teal-500 text-white py-3 px-6 rounded-md shadow-md">
-              Donate Now
-            </button>
+    <>
+      <section className="relative bg-[url(/assets/images/savelife.jpg)] py-16 bg-cover bg-center bg-no-repeat h-[300px] md:h-[400px]">
+        <div className="absolute inset-0 bg-gradient-to-r from-gray-900/95 via-gray-900/70 to-transparent"></div>
+        <div className="relative mx-auto max-w-screen-2xl px-4 py-28 sm:px-6 lg:flex lg:h-screen lg:px-8 text-white text-5xl font-bold max-md:text-3xl capitalize">
+          {cause.title}
+        </div>
+      </section>
+      <section className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+        {error ? (
+          <NotFoundPageCustom />
+        ) : (
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 md:items-center">
+            <div>
+              <Image
+                src={cause.image_url}
+                alt={cause.title}
+                width={800}
+                height={400}
+                className="rounded-lg"
+                priority
+              />
+            </div>
+            <div>
+              <h1 className="text-4xl font-bold mb-4">{cause.title}</h1>
+              <p className="text-gray-600 mb-4">{cause.category}</p>
+              <p className="mt-6 text-lg">{cause.description}</p>
+              <div className="mt-10">
+                <button className="bg-teal-500 text-white py-3 px-6 rounded-md shadow-md">
+                  Donate Now
+                </button>
+              </div>
+            </div>
           </div>
-        </>
-      )}
-    </div>
+        )}
+      </section>
+    </>
   );
 };
 
--- app/notfoundpage_custom/layout.tsx
+++
@@ -1,5 +0,0 @@
-// app/not-found/layout.tsx
-export default function NotFoundLayout({ children }: { children: React.ReactNode }) {
-    return <>{children}</>; // Render only the 404 content without the global layout
-  }
-  
