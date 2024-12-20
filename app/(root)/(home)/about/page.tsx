798d7684819eaa77223eb8b927dad4db35af42f0
fanyicharllson <fanyicharllson1@gmail.com>
2024-12-20 12:22:38 -0800

    Adjusted the path of the image

--- app/(root)/(home)/about/page.tsx
+++ app/(root)/(home)/about/page.tsx
@@ -61,7 +61,7 @@ export default function AboutPage() {
   return (
     <>
       {/* About Banner section */}
-      <section className="relative bg-[url(/assets/images/savelife.jpg)] py-16 bg-cover bg-center bg-no-repeat h-[300px] md:h-[400px]">
+      <section className="relative bg-[url('https://qbfyfenofkxhkgzanjlu.supabase.co/storage/v1/object/public/banner_image/savelife.jpg')] py-16 bg-cover bg-center bg-no-repeat h-[300px] md:h-[400px]">
         <div className="absolute inset-0 bg-gradient-to-r from-gray-900/95 via-gray-900/70 to-transparent"></div>
         <div className="relative mx-auto max-w-screen-2xl px-4 py-28 sm:px-6 lg:flex lg:h-screen lg:px-8 text-white text-5xl font-bold max-md:text-3xl capitalize">
           About Us
--- app/(root)/(home)/contact/page.tsx
+++ app/(root)/(home)/contact/page.tsx
@@ -65,7 +65,7 @@ export default function ContactPage() {
 
   return (
     <>
-      <section className="relative bg-[url(/assets/images/savelife.jpg)] py-16 bg-cover bg-center bg-no-repeat h-[300px] md:h-[400px]">
+      <section className="relative bg-[url('https://qbfyfenofkxhkgzanjlu.supabase.co/storage/v1/object/public/banner_image/savelife.jpg')] py-16 bg-cover bg-center bg-no-repeat h-[300px] md:h-[400px]">
         <div className="absolute inset-0 bg-gradient-to-r from-gray-900/95 via-gray-900/70 to-transparent"></div>
         <div className="relative mx-auto max-w-screen-2xl px-4 py-28 sm:px-6 lg:flex lg:h-screen lg:px-8 text-white text-5xl font-bold max-md:text-3xl capitalize">
           Contact Us
